09b27b7020b2b09f971107e67c94ad89d31ab4c2
Maciej Dobosz <dobosz.ma@interia.pl>
2018-06-26 20:07:15 +0200

    Front end changes

---
+++ app/controllers/concerns/profit_calculator.rb
@@ -0,0 +1,12 @@
+module ProfitCalculator extend ActiveSupport::Concern
+  def profit
+    won_array = Formula1.where('match_starts_at > ?', Date.today - 30)
+                          .where('status == ?', 1)
+                            .where('status != ?', 0).pluck(:odds)
+    lost_array = Formula1.where('match_starts_at > ?', Date.today - 30)
+                          .where('status >= ?', 2).pluck(:odds)
+    won = won_array.inject(0){ |sum, x| sum + x }
+    won -= lost_array.size
+    (10 * won).round(0)
+  end
+end
--- app/controllers/formula1_controller.rb
+++ app/controllers/formula1_controller.rb
@@ -1,5 +1,8 @@
 class Formula1Controller < ApplicationController
+  include ProfitCalculator
+
   def index
     @formula1s = Formula1.order('match_starts_at DESC').page(params[:page]).per(10)
+    @profit = profit
   end
 end
