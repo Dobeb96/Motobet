2a1da50b0db2fd60fcc3d2a502d14d4ef3709286
Maciej Dobosz <dobosz.ma@interia.pl>
2018-06-26 20:13:18 +0200

    Change for production environment

--- app/controllers/concerns/profit_calculator.rb
+++ app/controllers/concerns/profit_calculator.rb
@@ -1,7 +1,7 @@
 module ProfitCalculator extend ActiveSupport::Concern
   def profit
     won_array = Formula1.where('match_starts_at > ?', Date.today - 30)
-                          .where('status == ?', 1)
+                          .where('status = ?', 1)
                             .where('status != ?', 0).pluck(:odds)
     lost_array = Formula1.where('match_starts_at > ?', Date.today - 30)
                           .where('status >= ?', 2).pluck(:odds)
